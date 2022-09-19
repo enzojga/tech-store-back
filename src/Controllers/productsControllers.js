1a7e94a5c7807de226e8ee830b25a6bf6e2cb18e
Enzo <enzo202p@gmail.com>
2022-09-19 01:50:12 -0300

    Implemento da funçao getProductById

--- src/Controllers/productsControllers.js
+++ src/Controllers/productsControllers.js
@@ -1,26 +1,28 @@
 import db from '../database/db.js';
 
-async function creatProduct(req, res){
-    try{
-        console.log(req.body);
-        const {description, image, price, name, category} = req.body;
-        console.log(description, image, price, name, category);
-        await db.collection("products").insertOne({description, image, price, name, category, productId: Date.now()});
-        res.sendStatus(201);
-    }catch(err){
-        console.log(err);
-        res.sendStatus(422);
-    }
-}
-async function getProducts(req,res){
-    try{
+async function getProducts(req, res) {
+    try {
 
         const products = await db.collection("products").find().toArray();
         res.send(products);
 
-    }catch(err){
+    } catch (err) {
 
         console.log(err);
     }
 }
-export { creatProduct,getProducts }
+
+async function getProductsById(req, res) {
+    try {
+
+        const product = await db.collection("products").findOne({productId: Number(req.params.id)});
+        console.log(product)
+        res.send(product);
+
+    } catch (err) {
+
+        console.log(err);
+    }
+}
+
+export { getProducts, getProductsById }
