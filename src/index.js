62f785e34a67b3b2a904e7c378f05ebd01a8fcd6
Enzo <enzo202p@gmail.com>
2022-09-19 03:11:13 -0300

    Heroku bug 3

--- src/index.js
+++ src/index.js
@@ -8,12 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(productRouter);
 app.use(router);
+app.use(productRouter);
 
-
-
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.listen(port,()=>{
     console.log(`Server running on port ${port}`);
